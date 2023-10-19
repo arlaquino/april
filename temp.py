41b7815bc3b8c1131b7faf3c12d1a5b92c256728
Alwyn <arlaquino2003@gmail.com>
2023-10-19 15:51:46 -0600

    Step 2

--- temp.py
+++ temp.py
@@ -1,3 +1,12 @@
-# Program to convert Celsius to Fahrenheit
-fah = 25 * 9/5 + 32
-print(f"25 degrees is {fah} Fahrenheit")
+"""
+    Program to convert Celsius to Fahrenheit
+"""
+def cel_to_fah(celsius):
+    """ convert value given to fahrenheit"""
+    fah = celsius * 9/5 + 32
+    return fah
+
+
+celsius = 25
+fah = cel_to_fah(celsius)
+print(f"{celsius} degrees is {fah} Fahrenheit")
