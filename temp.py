292994a787f4001e0374578ddbb01b4fec0bad11
Alwyn <arlaquino2003@gmail.com>
2023-10-19 16:04:54 -0600

    step 3 - WIP

--- temp.py
+++ temp.py
@@ -6,7 +6,13 @@ def cel_to_fah(celsius):
     fah = celsius * 9/5 + 32
     return fah
 
+def fah_to_cel(fahrenheit):
+    celsius = fahrenheit - 32
+    return celsius
 
 celsius = 25
 fah = cel_to_fah(celsius)
 print(f"{celsius} degrees is {fah} Fahrenheit")
+
+fah = fah_to_cel(fah)
+print(f"{fah} degrees Fahrenheit is {celsius} celsius")
